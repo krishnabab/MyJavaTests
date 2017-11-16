17e68ebccc2721e96b17441b461d463fb260d277
krish <krish@DESKTOP-3E3D6CO.home>
2017-11-16 00:11:00 -0500

    commit gradle file

--- src/com/krish/First.java
+++ src/com/krish/First.java
@@ -1,9 +1,13 @@
 package com.krish;
 
+import org.joda.time.LocalTime;
+
 public class First {
-
 	public static void main(String[] args) {
-		System.out.println("Hello World@@@ KRISHNA ");
-	}
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
 
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+	}
 }
